ab0d0cd9bc121cb6287b5335a3b0d9400ba014e2
Jack Mason <jack.mason@posteo.net>
2019-02-18 12:33:28 +1000

    changed <tr> to <th>

--- src/components/TableHeaderRow.js
+++ src/components/TableHeaderRow.js
@@ -3,7 +3,7 @@ import React from 'react'
 class TableHeaderRow extends React.Component {
     render() {
         let headings = this.props.keys()
-        return <tr>{headings.map(name => <td>{name}</td>)}</tr>
+        return <th>{headings.map(name => <td>{name}</td>)}</th>
     }
 }
 
