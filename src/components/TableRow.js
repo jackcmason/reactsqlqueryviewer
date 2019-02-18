651e3d9bfa6ab335c3f2ef2186bb961e1f6a2183
Jack Mason <jack.mason@posteo.net>
2019-02-18 12:34:04 +1000

    added table row component

---
+++ src/components/TableRow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class TableRow extends React.Component {
+    render() {
+        let columns = this.props.values()
+        return <tr>{columns.map((value) => <td>{value}</td>)}</tr>
+    }
+}
+
+export default TableRow
